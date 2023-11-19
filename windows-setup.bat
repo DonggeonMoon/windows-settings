a801581cdbcdfa00303fb69f361b487144505b1f
DonggeonMoon <nullsafer@naver.com>
2023-11-19 23:26:07 +0900

    fix: 일부 패키지 source 변경

--- windows-setup.bat
+++ windows-setup.bat
@@ -1,24 +1,23 @@
 @echo off
 @chcp 65001
 
-winget install -e --id Google.Chrome
-winget install -e --id Mozilla.Firefox
+winget install "Windows Terminal" --source "msstore" --accept-package-agreements
+winget install "Ubuntu" --source "msstore" --accept-package-agreements
 
-winget install -e --id Microsoft.WindowsTerminal
-winget install -e --id Canonical.Ubuntu.2204
+winget install -e --id Git.Git --source winget --accept-package-agreements
+winget install -e --id OpenJS.NodeJS.LTS --accept-package-agreements
+winget install -e --id Python.Python.3.10 --accept-package-agreements
+winget install -e --id EclipseAdoptium.Temurin.8.JDK --accept-package-agreements
+winget install -e --id EclipseAdoptium.Temurin.11.JDK --accept-package-agreements
+winget install -e --id EclipseAdoptium.Temurin.17.JDK --accept-package-agreements
 
-winget install -e --id Git.Git --source winget
-winget install -e -id OpenJS.NodeJS.LTS
-winget install -e --id Python.Python.3.10
-winget install -e --id EclipseAdoptium.Temurin.8.JDK
-winget install -e --id EclipseAdoptium.Temurin.11.JDK
-winget install -e --id EclipseAdoptium.Temurin.17.JDK
+winget install -e --id Docker.DockerDesktop --accept-package-agreements
 
-winget install -e --id Docker.DockerDesktop
+winget install -e --id HeidiSQL.HeidiSQL --accept-package-agreements
 
-winget install -e --id HeidiSQL.HeidiSQL
+winget install -e --id Microsoft.VisualStudioCode --accept-package-agreements
 
-winget install -e --id Microsoft.VisualStudioCode
-
+winget install -e --id Google.Chrome --accept-package-agreements
+winget install -e --id Mozilla.Firefox --accept-package-agreements
 
 
